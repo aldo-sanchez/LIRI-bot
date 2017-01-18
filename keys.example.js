3be856353d9f04e2451288e19036ed62b99c2d2b
Aldo Sanchez <Aldo Sanchez>
2017-01-17 22:15:00 -0600

    :heavy_plus_sign: Added keys.example.js file

---
+++ keys.example.js
@@ -0,0 +1,8 @@
+// twitter key
+//For app to work, make sure to create an application in Twitter and fill in the keys given below:
+exports.keysTwitter = {
+  consumer_key: 'CONSUMER_KEY',
+  consumer_secret: 'CONSUMER_SECRET',
+  access_token_key: 'ACCESS_TOKEN_KEY',
+  access_token_secret: 'ACCESS_TOKEN_SECRET'
+};
