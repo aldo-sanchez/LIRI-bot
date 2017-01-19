eaa759379e3fde9a01c20b1db91c317a50ec8c52
Aldo Sanchez <Aldo Sanchez>
2017-01-18 18:02:50 -0600

    :construction: Successful my tweets

--- liri.js
+++ liri.js
@@ -5,8 +5,8 @@ var keysTwitter = importTwitter.keysTwitter;
 
 var client = new Twitter(keysTwitter);
 
-client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
-  //  console.log(tweets);
+client.get('statuses/user_timeline', {screen_name: '@willbeard4food', count: 10}, function(error, tweets, response) {
+   console.log(tweets);
 });
 
 // client.post('statuses/update', {status: 'Posting from node.js'}, function(error, tweet, response){
@@ -28,5 +28,5 @@ var url = 'http://www.omdbapi.com/?t=gladiator&y=&plot=short&r=json';
 
 request(url,function(error, response, body){
   if(error) throw error;
-  console.log(body)
+  // console.log(body)
 })
